bdf46469b7597a959a2eef114542077aed21e142
lelepton <leticyammedeiros@gmail.com>
2025-09-20 06:04:12 -0300

    feat: adiciona a estrutura básica do site

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnToggle = document.getElementById('btn-toggle');
+const menuMobile = document.getElementById('menu-mobile');
+
+btnToggle.addEventListener('click', () => {
+    if (menuMobile.classList.contains('show')) {
+        menuMobile.classList.remove('show');
+        btnToggle.innerHTML = `<i class="fa-solid fa-bars"></i>`
+    } else {
+        menuMobile.classList.add('show');
+        btnToggle.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+    }
+})
