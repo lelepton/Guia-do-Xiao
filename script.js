f10319c52139978ad5839a4ceca71e21f9d7fc93
lelepton <leticyammedeiros@gmail.com>
2025-10-06 23:17:42 -0300

    refactor: melhora o grid das seções, remove o slider, adiciona botão no form etc

--- script.js
+++ script.js
@@ -18,22 +18,3 @@ btnTema.addEventListener("click", () => {
         btnTema.innerHTML = `<i class="fa-solid fa-sun"></i>`;
     }
 })
-
-let currentIndex = 0;
-const galeria = document.querySelector('.galeria');
-const slides = document.querySelectorAll('.galeria img');
-const totalSlides = slides.length;
-
-function showSlide(index) {
-    galeria.style.transform = `translateX(-${index * 320}px)`;
-    slides.forEach(img => img.classList.remove('active'));
-    slides[index].classList.add('active');
-}
-
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    showSlide(currentIndex);
-}
-
-setInterval(nextSlide, 3000);
-showSlide(currentIndex);
